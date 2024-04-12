d4bf7f0b450cd98c663f683e045fe030437684cc
Jorge Assaf <jorgeassaf160@gmail.com>
2024-04-12 13:15:18 -0600

    Refactor main for routing configuration and screen imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/app_theme.dart';
-import 'package:widgets_app/presentation/screens/Buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screens/Cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/Home/home_screen.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,13 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       theme: AppTheme(selectedColor: 3).getTheme(),
-      home: const Scaffold(body: HomeScreen()),
-      routes: {
-        '/buttons': (context) => const ButtonsScreen(),
-        '/cards': (context) => const CardsScreen(),
-      },
     );
   }
 }
